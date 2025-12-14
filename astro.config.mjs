b718c30a2a3e50613707b7411263185f8ed633e1
Thanh Ha Khuong <khuongha.812@gmail.com>
2025-12-14 10:05:58 +0100

    restart from starter project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,37 +1,5 @@
 // @ts-check
-import { defineConfig } from "astro/config";
-import tailwindcss from "@tailwindcss/vite";
-import vitePluginSvgr from "vite-plugin-svgr";
-import react from "@astrojs/react";
-import sitemap from "@astrojs/sitemap";
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-  vite: {
-    plugins: [tailwindcss(), vitePluginSvgr({})],
-  },
-  devToolbar: {
-    enabled: false,
-  },
-  integrations: [react(), sitemap()],
-
-  markdown: {
-    shikiConfig: {
-      defaultColor: false,
-      themes: {
-        light: "github-light-high-contrast", // one-light
-        dark: "github-dark", // plastic
-      },
-      wrap: true,
-    },
-  },
-
-  prefetch: {
-    prefetchAll: true,
-    // defaultStrategy: "load",
-  },
-
-  output: "static",
-  site: "https://khwowng.github.io",
-  base: '/',
-});
+export default defineConfig({});
